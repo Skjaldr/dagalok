cb6267c7b9383636e614f298c1135fb6c944a258
Skjaldr <sthines86@gmail.com>
2026-06-08 11:26:37 -0500

    adding comments to input.rs

--- client/src/player/input.rs
+++ client/src/player/input.rs
@@ -1,4 +1,4 @@
-use bevy::{math::VectorSpace, prelude::*};
+use bevy::prelude::*;
 use bevy_third_person_camera::ThirdPersonCamera;
 
 use crate::{characters::setup_char::{Position, Speed, IsMoving}, player::setup_player::Player};
@@ -26,28 +26,32 @@ pub fn character_movement(
     position.0 = Vec3::ZERO;
     moving.0 = false;
 
+    // if the player pressed a button WASD, the character moves in the direction the player indicates and also faces that direction.
+    // the character can also pan the camera around with the mouse, changing the forward heading.  If the player holds the forward button "W"
+    // while panning the camera, the character's direction changesn to indicate the new forward camera position is forward.
     if input.pressed(KeyCode::KeyW) {
-        // moving.0 = true;
         position.0 += *cam.forward()
     }
     if input.pressed(KeyCode::KeyS) {
-        // moving.0 = true;
         position.0 += *cam.back()
     }
     if input.pressed(KeyCode::KeyA) {
-        // moving.0 = true;
         position.0 += *cam.left()
     }
     if input.pressed(KeyCode::KeyD) {
-        // moving.0 = true;
         position.0 += *cam.right()
     }
 
+    // set the position of y to 0.0 to lock the player's Y axis.
     position.0.y = 0.0;
 
+    // create a delta that takes the normalized position and multiplies it by the current time and the speed of the player.
     let delta = position.0.normalize_or_zero() * time.delta_secs() * speed.0;
     transform.translation.x += delta.x;
     transform.translation.z += delta.z;
+
+    // We want our character's position to stay the same as the direction they were last facing.  We also want to tell the IsMoving component that
+    // the character is moving, if they're moving.
     if position.0.length_squared() > 0.0 {
         transform.look_to(position.0, Vec3::Y);
         moving.0 = true;
