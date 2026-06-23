7f4978e7ed504d5bcc69a32c6328d5c1329bc9c8
Skjaldr <Strazku@gmail.com>
2026-06-23 10:01:25 -0500

    basic spacetimedb implementation with help from impatient programmers guide

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use avian3d::prelude::*;
 use bevy_third_person_camera::ThirdPersonCameraPlugin;
 
-use crate::{characters::CharacterPlugin, gamestate::GameStatePlugin, npc::NonPlayerCharacterPlugin, player::PlayerPlugin, setup::SetupPlugin, terrain::TerrainPlugin, ui::UiPlugin};
+use crate::{characters::CharacterPlugin, gamestate::GameStatePlugin, networking::NetworkingPlugin, npc::NonPlayerCharacterPlugin, player::PlayerPlugin, setup::SetupPlugin, terrain::TerrainPlugin, ui::UiPlugin};
 
 mod setup;
 mod gamestate;
@@ -11,12 +11,15 @@ mod terrain;
 mod player;
 mod ui;
 mod npc;
+mod networking;
+mod module_bindings;
 
 fn main() {
     let mut app = App::new();
     app
         .add_plugins((
             DefaultPlugins,
+            NetworkingPlugin,
             PhysicsPlugins::default(),
             ThirdPersonCameraPlugin,
             GameStatePlugin,
